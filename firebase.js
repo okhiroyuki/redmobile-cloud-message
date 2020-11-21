09bc2fcc95e26c310934773185110237834df619
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2020-11-21 17:04:44 +0900

    add function

---
+++ firebase.js
@@ -0,0 +1,30 @@
+const admin = require('firebase-admin');
+
+function init(){
+    let serviceAccount;
+
+    if(process.env.SERVICE_ACCOUNT_KEY){
+        serviceAccount = process.env.SERVICE_ACCOUNT_KEY;
+    }else{
+        serviceAccount = require("./serviceAccountKey.json");
+    }
+    
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: "https://redmobile-1c98d.firebaseio.com"
+    });    
+}
+
+async function sendMessage(message){
+    try{
+        const res = await admin.messaging().send(message);
+        return Promise.resolve(res);
+    }catch(error){
+        return Promise.reject(error);
+    }
+}
+
+module.exports = {
+    init: init,
+    sendMessage: sendMessage,
+};
---
+++ init.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const firebase = require('./firebase');
+
+firebase.init();
+
+app.set('port', (process.env.PORT || 3000));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.post('/v1/message/send', async (request, response) => {
+    const message = request.body;
+    console.log(message);
+    try{
+        const result = await firebase.sendMessage(message);
+        response.send(result);
+    }catch(error){
+        response
+            .status(500)
+            .send(error.message);
+    }    
+});
+
+app.listen(app.get('port'), () => {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
