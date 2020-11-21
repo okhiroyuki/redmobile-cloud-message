c6344a160bfaeaf77839604210d2bee470583191
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2020-11-21 17:27:15 +0900

    update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,7 +4,7 @@ function init(){
     let serviceAccount;
 
     if(process.env.SERVICE_ACCOUNT_KEY){
-        serviceAccount = process.env.SERVICE_ACCOUNT_KEY;
+        serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
     }else{
         serviceAccount = require("./serviceAccountKey.json");
     }
